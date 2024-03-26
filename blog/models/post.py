d1e167cd93f44aea1b17b32cffa978e07dfa7e04
umar <umar.m.sheikh@gmail.com>
2024-03-26 15:03:05 +0500

    Add post model

---
+++ blog/models/post.py
@@ -0,0 +1,19 @@
+from .base import *
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    tagline = models.CharField(max_length=100)
+    meta_title = models.CharField(max_length=200, null=True, blank=True)
+    slug = models.SlugField()
+    summary = models.CharField(max_length=255)
+    content = models.TextField()
+    published = models.BooleanField(default=False)
+    published_at = models.DateTimeField(null=True, blank=True)
+
+    parent_post = models.ForeignKey('self', on_delete=models.SET_NULL, blank=True, null=True, related_name='child_posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
